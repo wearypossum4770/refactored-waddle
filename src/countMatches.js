185db4533c35d8dfa84da8f3cdaaa49ee692850d
Stephen Smith <wearypossum4770@yahoo.com>
2021-05-14 21:00:41 -0400

    refacor: attempt js leet-code challenge 1773
    Refactor
    Refactor js attempt of leet-code challenge 1773.
    |Runtime|percentile|Memory Usage|percentile|Runtime Diff|Memory Diff|
    | ---- | ---- | ---- | ---- | ---- | ---- |
    |  108 ms | 6.18% |43.9 MB | 6.99%| | |
    |	84 ms|94.64%|42.4 MB|71.33%| | |
    
    Commit 78c33ee3957979031e4479b63fa0282a33a0ff45

--- src/countMatches.js
+++ src/countMatches.js
@@ -7,15 +7,19 @@
  * */
 export default function countMatches(items, ruleKey, ruleValue) {
   let count = 0;
-  items.forEach((array) => {
-    if (ruleKey === "type" && array[0] === ruleValue) {
-      count += 1;
-    } else if (ruleKey === "color" && array[1] === ruleValue) {
-      console.log("type");
-      count += 1;
-    } else if (ruleKey === "name" && array[2] === ruleValue) {
-      count += 1;
-    }
-  });
+  function counter(position) {
+    items.forEach((item) => (item[position] === ruleValue ? count++ : count));
+  }
+  switch (ruleKey) {
+    case "type":
+      counter(0);
+      break;
+    case "color":
+      counter(1);
+      break;
+    default:
+      counter(2);
+      break;
+  }
   return count;
 }
