e021cca4ba43f4cdb8eda7a2c96c099ca3b0f4a6
Stephen Smith <wearypossum4770@yahoo.com>
2021-05-15 23:50:05 -0400

    feat: attempt js leet-code challlenge 1480

---
+++ src/runningSum.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const reducer = (array) => array.reduce((accum, currVal) => accum + currVal);
+export default function runningSum(nums) {
+  let results = Array(nums.length);
+  for (let i = 0; i < nums.length; i++) {
+    results[i] = reducer(nums.slice(0, i + 1));
+  }
+  return results;
+}
