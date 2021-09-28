6ff60fffb42e53d0793d2e53dd3d9930f8c8f08b
stephen smith ecs <wearypossum4770@yahoo.com>
2021-09-28 11:44:12 -0400

    completes sort sentence challenge in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use crate::count_matches::count_matches;
+use crate::kids_with_candies::kids_with_candies;
+use crate::maximum_wealth::maximum_wealth;
+use crate::defang_ip_addr::defang_ip_addr;
+use crate::running_sum::running_sum;
+use crate::shuffle::shuffle;
+use crate::sort_sentence::sort_sentence;
+mod sort_sentence;
+mod shuffle;
+mod running_sum;
+mod maximum_wealth;
+mod kids_with_candies;
+mod defang_ip_addr;
+mod count_matches;
 fn main() {
-    println!("Hello, world!");
+    let vec_u8 = vec![maximum_wealth(vec![vec![1, 2, 3], vec![3, 2, 1]])];
+    let vec_vector_i32 = vec![shuffle(vec![2, 5, 1, 3, 4, 7], 3)];
+    let vec_vector_u32 = vec![running_sum(&[1, 2, 3, 4])];
+    let vec_vector_bool = vec![kids_with_candies(vec![2, 3, 5, 1, 3], 3)];
+let vec_u32 = vec![count_matches(
+    &[
+        ["phone", "blue", "pixel"],
+        ["computer", "silver", "phone"],
+        ["phone", "gold", "iphone"]
+    ],
+    "type",
+    "phone"
+),];
+let vec_string = vec![defang_ip_addr("1.1.1.1"),sort_sentence("is2 sentence4 This1 a3")];
+println!("{:?}",vec_u32);
+println!("{:?}",vec_string);
+println!("{:?}",vec_vector_bool);
+println!("{:?}",vec_u8);
+println!("{:?}",vec_vector_u32);
+println!("{:?}",vec_vector_i32);
+println!("Hello, world!");
 }
--- src/sort_sentence.rs
+++ src/sort_sentence.rs
@@ -1,5 +1,12 @@
-pub fn sort_sentence(sentence: &str) -> &str{
-    
+use regex::Regex;
+pub fn sort_sentence(sentence: &str) ->String{
+    let re = Regex::new(r"(\w+)(\d+)").unwrap();
+    let mut target: Vec<&str> = sentence.split(" ").collect();
+    for capture in re.captures_iter(sentence){
+        let   location = capture[2].parse::<usize>().unwrap();
+        target[location-1] = &capture.get(1).unwrap().as_str();
+    }
+    target.join(" ")
 }
 #[cfg(test)]
 mod tests {
