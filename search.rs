58f629864c12dd85d06722f2733c9eaa54e9944c
Stephen Smith <50812322+wearypossum4770@users.noreply.github.com>
2022-03-04 00:53:23 -0500

    Update search.rs

--- search.rs
+++ search.rs
@@ -1,5 +1,5 @@
 pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-  let mut result:i32 = 0;
+  let mut result:i32 = -1;
   for (index, value) in nums.iter().enumerate() {
     if value == &target {
       result = index as i32;
@@ -7,3 +7,12 @@ pub fn search(nums: Vec<i32>, target: i32) -> i32 {
    }
   result
 }
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so
